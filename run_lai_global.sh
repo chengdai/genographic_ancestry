72e6653900396298bef3bb3b9548897f2bd2e81f
Cheng Dai <chengdai@mit.edu>
2019-06-20 04:02:56 -0400

    cleanup

--- run_lai_global.sh
+++
@@ -1,5 +0,0 @@
-python ancestry_pipeline/lai_global.py \
---bed_list ./ibd_bed_list.txt \
---ind_list ./AMR_AFR_subset/ibd_cluster_classified/NG_ibd_classified_AMR_AFR.txt \
---pops EUR,AFR,AMR \
---out ./AMR_AFR_subset/ibd_cluster_classified/global_ancestry/all_lai_global.txt
